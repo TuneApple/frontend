4e43abb7f06674cf8414bf5339c0dea190f57637
tuneapple <tune.backend@gmail.com>
2022-09-06 08:33:06 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import Header from "./components/header/Header";
+import {AuthContext, RegionContext, CartContext} from "../src/context/index";
+import AppRouter from "./router";
 
 function App() {
+    const [isAuth, setIsAuth] = useState(false);
+    const [region, setRegion] = useState(null);
+    const [cart, addCart] = useState(null);
+
+
+    useEffect(() => {
+        if (!localStorage.getItem('region')) {
+            localStorage.setItem("region", 'Санкт-Петербург')
+            setRegion('Санкт-Петербург')
+
+        }else {
+            setRegion(localStorage.getItem('region'))
+        }
+    }, []);
+
+    useEffect( () => {
+        if (localStorage.getItem('auth')) {
+            setIsAuth(true);
+        }
+    }, [])
+
+    useEffect( () => {
+        if (localStorage.getItem('cart')) {
+        } else if (!localStorage.getItem('cart')) {
+            addCart({})
+            localStorage.setItem("cart", '{}')
+        }
+    }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <AuthContext.Provider value={{
+            isAuth,
+            setIsAuth
+        }}>
+            <RegionContext.Provider value={{
+                region,
+                setRegion
+            }}>
+                <CartContext.Provider value={{
+                    cart,
+                    addCart,
+                }}>
+                    <Header/>
+                    <AppRouter/>
+                </CartContext.Provider>
+
+
+            </RegionContext.Provider>
+        </AuthContext.Provider>
+
     </div>
   );
 }
